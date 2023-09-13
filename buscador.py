66051ff58e14b6f02b0398d81bfe4c21a6cef93b
Luis Candido <103050923+luiseduardofc1500@users.noreply.github.com>
2023-09-13 15:08:04 -0300

    Update buscador.py

--- buscador.py
+++ buscador.py
@@ -7,50 +7,50 @@ from multiprocessing import Pool
 
 def search_pcr(args):
     index, pcr = args 
-    achou = False
+    found = False
     p = re.compile(pcr)
-    results = []  # Store the results for this PCR product
-    for i in SeqIO.parse("arraia.fasta", "fasta"):
+    results = [] 
+    for i in SeqIO.parse("caminho/do/arquivo", "fasta"):
         seqstr = str(i.seq)
         matches = p.finditer(seqstr)
         for m in matches:
             if m.group():
-                achou = True
+                found = True
                 start, end = m.span()
                 id = i.id  # Get the sequence ID
-                results.append((index, start, end, id))  # Append (index, start, end, id) to results
+                results.append((index, start, end, id))  # If need more information to put future in table add here
                 print(i)
                 print("{start} {match} {end} da linha {index}".format(start=start, match=m.group(), end=end, index=index))
 
-    print(f"{index}, {achou}")  
-    return results  # Return a list of results
+    print(f"{index}, {found}")  
+    return results  
 
 def main():
-    tabela = pd.read_csv('cortadinho1.csv', sep=";")
-    tabela['find'] = 0
-    tabela['id'] = ""
-    tabela['start'] = 0  
-    tabela['end'] = 0
+    table = pd.read_csv('arquivo.csv')
+    table['find'] = 0
+    table['id'] = ""
+    table['start'] = 0  
+    table['end'] = 0
     start_clock = datetime.datetime.now()
     
     # Define the number of processes to be used (adjust as needed)
     num_processes = 4
     
     with Pool(num_processes) as pool:
-        indices_geral = range(len(tabela['PCR Product']))
-        pcr_list = tabela['PCR Product'].tolist()
+        indices_geral = range(len(table['PCR Product']))
+        pcr_list = table['PCR Product'].tolist()
         results_list = pool.map(search_pcr, zip(indices_geral, pcr_list))
         
         for aux, results in enumerate(results_list):
             if results:
-                achou = 1
-                index, start, end, id = results[0]  # Take the first result
-                tabela.at[aux, 'find'] = achou
-                tabela.at[aux, 'id'] = id
-                tabela.at[aux, 'start'] = start
-                tabela.at[aux, 'end'] = end
+                found = 1
+                index, start, end, id = results[0] 
+                table.at[aux, 'find'] = found
+                table.at[aux, 'id'] = id
+                table.at[aux, 'start'] = start
+                table.at[aux, 'end'] = end
 
-    tabela.to_csv('output.csv', index=False)
+    table.to_csv('output.csv', index=False)
     end_clock = datetime.datetime.now()
     print(end_clock - start_clock)
 
